daeb5b745ec69607591b3ea5cf380857054f1632
KalaRohit <59739808+KalaRohit@users.noreply.github.com>
2021-11-28 00:37:23 -0600

    Add files via upload

---
+++ onlinepainter.py
@@ -0,0 +1,10 @@
+from flask import Flask, url_for, request, render_template
+
+app = Flask(__name__)
+@app.route("/", methods=['GET', 'POST'])
+def main_page():
+    if request.method == 'POST':
+        user = request.form['username']
+        password = request.form['password']
+        print(user, password)
+    return render_template("basepage.html")
---
+++ static/utility.js
@@ -0,0 +1,24 @@
+function bringUpForm(){
+    //commonly accessed elements
+    blackFade = document.getElementById("blackFade");
+    popupForm = document.getElementById("popup");
+    popupForm.style.display="block";
+    blackFade.style.display="block";
+}
+
+function hideForms(){
+    //commonly accessed elements
+    blackFade = document.getElementById("blackFade");
+    popupForm = document.getElementById("popup");
+    popupForm.style.display="none";
+    blackFade.style.display="none";
+}
+
+function signInFormValidation(){
+    console.log("test");
+
+}
+
+function transitionToSignUp(){
+    
+}
